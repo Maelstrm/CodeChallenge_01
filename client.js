e19bdc1a103708eb21f28fd9f36365e802f738a3
Jakeh Clark <jakehclarkcreates@gmail.com>
2018-07-30 14:17:15 -0500

    completed assignment

--- client.js
+++ client.js
@@ -5,70 +5,70 @@
 // [x] any div created with the generate click should have a red background
 // [x] "Swap" should change its parent background from red to yellow.
 // [x] "Swap" again should turn the parent background color back to red
-// "Delete" should remove its parent div and all of its contents
+// [x] "Delete" should remove its parent div and all of its contents
 
 
-// store global variables here
+// Store global variables here
 let totalClicks = 0;
 
-// keeps track of the clickDiv background color
+// Keeps track of the clickDiv background color
 let red = true;
 
-// waits for page to initialize 
+// Waits for page to initialize
 $(readyNow);
 
-// when the page is ready, run these functons
+// When the page is ready, run these functons
 function readyNow() {
     eventHandlers();
-} // readyNow end
+} // ReadyNow end
 
 
-// this is where handlers go.
+// This is where handlers go.
 function eventHandlers() {
     $('#generateButton').on('click', generateClick);
-} // eventHandlers end
+} // EventHandlers end
 
-// generate Click
+// Generate Click
 function generateClick() {
 
-    // updates counter
+    // Updates counter
     clickCounter();
 
-    //clears the div that holds the counter and its buttons
+    //Clears the div that holds the counter and its buttons
     $('#clickDiv').remove;
     $('#generateParent').append('<div id="clickDiv"></div>');
     $('#clickDiv').html('<p>' + totalClicks + '</p>');
 
-    //appends "Swap" & "Delete" buttons
+    //Appends "Swap" & "Delete" buttons
     $('#clickDiv').append('<button id="swapButton">Swap</button>' + '<button id="deleteButton">Delete</button>');
 
-    // reloads the event handler for "Delete" and "Swap"
+    // Reloads the event handler for "Delete" and "Swap"
     clickDeleteHandler();
-} // end generateclick
+} // End generateclick
 
-// tracks the number of clicks
+// Tracks the number of clicks
 function clickCounter() {
     totalClicks += 1;
     return totalClicks;
-} // end clickCounter
+} // End clickCounter
 
 
 function clickDeleteHandler() {
-    // eventHandlers for "Swap" and "Delete"
+    // EventHandlers for "Swap" and "Delete"
     $('#swapButton').on('click', swapButtonClick);
     $('#deleteButton').on('click', deleteButtonClick);
-}
+} // End clickDeleteHandler
 
 function swapButtonClick() {
 
-    // if the value of red is true...
+    // If the value of red is true...
     if (red) {
         $('#clickDiv').css('background-color', 'yellow');
         red = false;
         return red;
     }
 
-    // if the value of red is not true
+    // If the value of red is not true
     else {
         $('#clickDiv').css('background-color', 'red');
         red = true;
@@ -81,7 +81,7 @@ function deleteButtonClick() {
     // Deletes the generate div
     $('#clickDiv').remove();
 
-    // clears the counter
+    // Resets the counter
     totalClicks = 0;
     return totalClicks;
-}
+} // End deleteButtonClick
